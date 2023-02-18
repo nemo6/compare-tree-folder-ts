8f1dd1d41e66e8a53963afc0093951cb3da9898d
nemo6 <miguel.maudet.contact@gmail.com>
2023-02-18 19:12:54 +0100

    Update record.ts

--- record.ts
+++ record.ts
@@ -1,9 +1,9 @@
 (obj2 as Record<typeof k, typeof k>)
-obj = obj as unknown as { prop: string };
-var obj: {[k: string]: any} = {};
+obj = obj as unknown as { prop: string }
+var obj: {[k: string]: any} = {}
 
-const someObj:ObjectType = data;
-const field = 'username';
+const someObj:ObjectType = data
+const field = 'username'
 
 const temp = someObj[field];
 const temp = someObj[field as keyof ObjectType]
